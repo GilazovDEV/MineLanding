a550687da40dcddef8a3cc7efcec524d82b5dec2
GilazovDEV <azalialebedeva6@gmail.com>
2024-09-28 21:27:22 +0500

    scroll

--- src/js/script.js
+++ src/js/script.js
@@ -1,6 +1,6 @@
 window.addEventListener("scroll", function () {
   const hiddenElement = document.getElementById("hiddenElement");
-  if (window.scrollY > 400) {
+  if (window.scrollY >= 400) {
     hiddenElement.classList.add("bg-l1ght");
   } else {
     hiddenElement.classList.remove("bg-l1ght");
