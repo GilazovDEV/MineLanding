8bfe8b815ed4a3b4d7861192523c3f22896eb2ee
GilazovDEV <azalialebedeva6@gmail.com>
2024-09-28 22:31:48 +0500

    add animation

--- src/js/script.js
+++ src/js/script.js
@@ -6,3 +6,24 @@ window.addEventListener("scroll", function () {
     hiddenElement.classList.remove("bg-l1ght");
   }
 });
+
+function onEntry(entry) {
+  entry.forEach(change => {
+    if (change.isIntersecting) {
+     change.target.classList.add('element-show');
+    }
+  });
+}
+
+let options = {
+  threshold: [0.3] };
+let observer = new IntersectionObserver(onEntry, options);
+let elements = document.querySelectorAll('.element-animation');
+
+for (let elm of elements) {
+  observer.observe(elm);
+}
+
+document.getElementById("confetti-button").addEventListener("click", function() {
+confetti();
+});
